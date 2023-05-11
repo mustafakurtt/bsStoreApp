2e2c5897bbaad09d2507947b8f620de77a24c860
0jarvi3 <mustafa.kurt.0@icloud.com>
2023-05-11 08:37:16 +0300

    RepositoryManager

---
+++ Repositories/EFCore/RepositoryManager.cs
@@ -0,0 +1,19 @@
+﻿using Repositories.Contracts;
+
+namespace Repositories.EFCore;
+
+public class RepositoryManager : IRepositoryManager
+{
+    private readonly RepositoryContext _repositoryContext;
+
+    public RepositoryManager(RepositoryContext repositoryContext)
+    {
+        _repositoryContext = repositoryContext;
+    }
+
+    public IBookRepository Book => new BookRepository(_repositoryContext);
+    public void Save()
+    {
+        _repositoryContext.SaveChanges();
+    }
+}
