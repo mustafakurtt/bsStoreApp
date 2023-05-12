6f299c1440927da8411318bd997fdfd788eb5bb2
0jarvi3 <mustafa.kurt.0@icloud.com>
2023-05-12 15:10:57 +0300

    Error

---
+++ WebAPI/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using Entities.ErrorModels;
+using Microsoft.AspNetCore.Diagnostics;
+using Services.Contracts;
+
+namespace WebAPI.Extensions;
+
+public static class ExceptionMiddlewareExtensions
+{
+    public static void ConfigureExceptionHandler(this WebApplication app, ILoggerService logger)
+    {
+        app.UseExceptionHandler(appError =>
+        {
+            appError.Run(async context =>
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+
+                var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                if (contextFeature is not null)
+                {
+                    logger.LogError($"Something went wrong:{contextFeature.Error}");
+                    await context.Response.WriteAsync(new ErrorDetails() { StatusCode = context.Response.StatusCode, Message = "Internal Server Error" }.ToString());
+                }
+            });
+        });
+    }
+}
